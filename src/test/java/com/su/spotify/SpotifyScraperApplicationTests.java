8eb1e89d50d84aa66da91eb6ed6e77910c3cef9d
Surya Sanjiv <suryasanjiv@hotmail.com>
2023-07-22 13:36:32 +0200

    first commit

---
+++ src/test/java/com/su/spotify/SpotifyScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.su.spotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
